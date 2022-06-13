f8536d0b1ac4bd3fd7f55ad1e17d0981f27efd3c
Raul Vazquez <raul.vazquez.96@gmail.com>
2022-06-13 03:35:30 +0200

    iniciacion springboot

---
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -0,0 +1,31 @@
+package com.bolsadeideas.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bolsadeideas.springboot.web.app.models.Usuario;
+
+@Controller
+@RequestMapping("/app")
+public class IndexController {
+
+	@GetMapping({"/index","/",""})
+	
+	public String index(Model model) {
+		model.addAttribute("titulo", "Este es el titulo de la pagina Index");
+		return "index";
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario(); //Instanciamos el usuario
+		usuario.setNombre("Raul");
+		usuario.setApellido("Vazquez");
+		usuario.setEmail("raul@gmail.com");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil de: "+(usuario.getNombre()+" "+(usuario.getApellido())));
+		return "perfil";
+	}
+}
